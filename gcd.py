f1d75222a3ae7535f6f7512dbcb8306475793c0f
Aryan4646 <aryansha2004@gmail.com>
2025-03-03 15:55:41 +0530

    Fixed GCD logic - ensured highest common divisor

--- gcd.py
+++ gcd.py
@@ -1,23 +1,13 @@
 # WAP to calculate GCD without using any inbuilt function
 
-num1 = int(input("Enter the first number :"))
-num2 = int(input("Enter the second number :"))
+num1 = int(input("Enter the first number :"))  #Take input 1 from the user
+num2 = int(input("Enter the second number :"))  #Take input 2 from the user
 gcd = 1
-i = 2
-if num1 > num2:
-    while num2 >= i :
-        if num1 % i == 0 and num2 % i == 0 :
-            gcd *= i
-            num1 //= i
-            num2 //= i
-        else:
-            i += 1
+if num1 >num2:  # Condition check if number is greater because we need to run loop only until then
+    num3 = num2
 else:
-    while num1 >= i :
-        if num1 % i == 0 and num2 % i == 0 :
-            gcd *= i
-            num1 //= i
-            num2 //= i
-        else:
-            i += 1
-print(f"GCD of {num1*gcd} & {num2*gcd} is: {gcd}")
+    num3 = num1
+for i in range(1, num3+1):
+    if num1 % i == 0 and num2 % i == 0:
+        gcd = i  # modify the gcd
+print(f"GCD of {num1} & {num2} is: {gcd}")
