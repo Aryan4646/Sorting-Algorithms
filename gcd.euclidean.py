76e0b5409a5e587bf130d6ba84bd42f7f5cf4781
Aryan4646 <aryansha2004@gmail.com>
2025-03-03 16:38:09 +0530

    Added GCD program with euclidean algorithm and recursive call with implementation

--- gcd.euclidean.py
+++ gcd.euclidean.py
@@ -1,9 +1,12 @@
-def gcd(a,b):
+def gcd(a, b, count = 0):
+    count += 1
     if b == 0:
-        return a
-    return gcd(b, a % b)
-
+        print(f"Step {count}: {a,b}")
+        return a, count
+    print(f"Step {count}: {a,b}")
+    return gcd(b, a % b, count)
 num1 = int(input("Enter the larger number: "))
 num2 = int(input("Enter the smaller number: "))
-
-print(f"GCD of {num1} & {num2} is : {gcd(num1,num2)}")
+gcd, calls = gcd(num1,num2)
+print(f"GCD of {num1} & {num2} is : {gcd}")
+print(f"Number of recursive call is: {calls}")
