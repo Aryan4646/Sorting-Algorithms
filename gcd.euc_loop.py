13dbfbd1bb8353a20a69167335f9826a50bcf03f
Aryan4646 <aryansha2004@gmail.com>
2025-03-03 17:04:23 +0530

    Updated iterative approach

--- gcd.euc_loop.py
+++ gcd.euc_loop.py
@@ -1,15 +1,8 @@
 def gcd(num1,num2):
-    if num1 > num2:
         while num2 != 0:
             num1, num2 = num2, (num1 % num2)
-        else:
             gcd = num1
-    else:
-        while num1 != 0:
-            num2, num1 = num1, (num2 % num1)
-        else:
-            gcd = num2
-    return gcd
+        return gcd
 num1 = int(input("Enter the first number :"))  #Take input 1 from the user
 num2 = int(input("Enter the second number :"))  #Take input 2 from the user
 
