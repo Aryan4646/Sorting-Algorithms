10c1dc6b86e4b06248b44dd28dfda07d7e287e12
Aryan4646 <aryansha2004@gmail.com>
2025-03-03 15:59:25 +0530

    Optimized further

--- gcd.py
+++ gcd.py
@@ -3,10 +3,7 @@
 num1 = int(input("Enter the first number :"))  #Take input 1 from the user
 num2 = int(input("Enter the second number :"))  #Take input 2 from the user
 gcd = 1
-if num1 >num2:  # Condition check if number is greater because we need to run loop only until then
-    num3 = num2
-else:
-    num3 = num1
+num3 = num1 if num1 > num2 else num2   #Checks the smaller number
 for i in range(1, num3+1):
     if num1 % i == 0 and num2 % i == 0:
         gcd = i  # modify the gcd
