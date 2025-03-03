612d65ade33175dfdc75e2f355f56ed48250074b
Aryan4646 <aryansha2004@gmail.com>
2025-03-03 16:40:44 +0530

    Improved recursion tracking and variable naming in Euclidean GCD

--- gcd.euclidean.py
+++ gcd.euclidean.py
@@ -7,6 +7,6 @@ def gcd(a, b, count = 0):
     return gcd(b, a % b, count)
 num1 = int(input("Enter the larger number: "))
 num2 = int(input("Enter the smaller number: "))
-gcd, calls = gcd(num1,num2)
+gcd_value, calls = gcd(num1,num2)
 print(f"GCD of {num1} & {num2} is : {gcd}")
 print(f"Number of recursive call is: {calls}")
