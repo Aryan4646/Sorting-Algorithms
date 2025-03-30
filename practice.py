ceeb427a4ab2cdd5b0f29ab5c855de323c4d0e6e
Aryan4646 <aryansha2004@gmail.com>
2025-03-30 23:39:40 +0530

    Updated code in practice.py

--- practice.py
+++ practice.py
@@ -299,3 +299,8 @@
 # rec(n)
 
 # Implement a recursive function to compute the nth Fibonacci number.
+def rec(n):
+    if n
+n = int(input("Enter the number:"))
+
+print(f"The {n}th number of the fibonacci sequence is : {rec(n)}")
