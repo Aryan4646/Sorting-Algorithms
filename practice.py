695c1fadb2713baf73cca51140df7d894bc2619e
Aryan4646 <aryansha2004@gmail.com>
2025-04-03 03:39:52 +0530

    Updated code in practice.py

--- practice.py
+++ practice.py
@@ -937,14 +937,14 @@ if num1 > num2:
     for i in range(1,num1):
         if num1 % i == 0 and num2 % i == 0:
             gcd *= i
-            num1 /= i
-            num2 /= i
+            num1 //= i
+            num2 //= i
 else:
     for i in range(1,num2):
         if num1 % i == 0 and num2 % i == 0:
             gcd *= i
-            num1 /= i
-            num2 /= i
+            num1 //= i
+            num2 //= i
 print(gcd)
 
 
