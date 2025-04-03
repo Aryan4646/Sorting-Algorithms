b57f0087c6988ed0176ea6f5a4000d705a644995
Aryan4646 <aryansha2004@gmail.com>
2025-04-03 21:33:15 +0530

    Added sorting.py

--- sorting.py
+++ sorting.py
@@ -1,1 +1,15 @@
 
+# Bubble sort
+def bubble_sort(l):
+    for x in range():
+
+
+
+
+n = int(input("Enter the number of elements you would like to add in list: "))
+l = []
+for i in range(n):
+    p = int(input(f"Enter the element at {i} index in the list: "))
+    l.append(p)
+print(f"Original list is : {l}")
+print(f"Sorted list is: {bubble_sort(l)}")
