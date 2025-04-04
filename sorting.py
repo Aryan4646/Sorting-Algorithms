4551471bce56ebf4953363fb6c8fb0f387f04f63
Aryan4646 <aryansha2004@gmail.com>
2025-04-04 11:27:48 +0530

    Added sorting.py

--- sorting.py
+++ sorting.py
@@ -1,22 +1,16 @@
 
 # Bubble sort
-def bubble_sort(l,mi,mx):
-    while l[0] != mi  and [len(l)-1] != mx:
-        for x in range(len(l)-1):
-            if l[x] > l[x+1]:
-                p = l[x]
-                l[x] = l[x+1]
-                l[x+1] = p
-            elif l[x] <= l[x+1]:
-                continue
-            print(l)
+def bubble_sort(l):
+    n = len(l)-1
+    for i in range(n-1):
+       for j in range(n-1-i):
+           if l[j] > l[j+1]:
+               l[j], l[j+1] = l[j+1],l[j]
     return l
 n = int(input("Enter the number of elements you would like to add in list: "))
 l = []
 for i in range(n):
     p = int(input(f"Enter the element at {i} index in the list: "))
     l.append(p)
-mi = min(l)
-mx = max(l)
 print(f"Original list is : {l}")
-print(f"Sorted list is: {bubble_sort(l,mi,mx)}")
+print(f"Sorted list is: {bubble_sort(l)}")
