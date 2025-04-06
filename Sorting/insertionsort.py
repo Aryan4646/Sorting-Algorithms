6b23bde5932bcac2ddba4d9e839630168324844f
Aryan4646 <aryansha2004@gmail.com>
2025-04-06 12:56:52 +0530

    Added insertionsort version

--- Sorting/insertionsort.py
+++ Sorting/insertionsort.py
@@ -8,10 +8,11 @@ def ins_sort(l):
             else:
                 l[j], l[j-1] = l[j-1], l[j]
     return l
+
 n = int(input("Enter the number of elements you would like to add in list: "))
 l = []
 for i in range(n):
     p = int(input(f"Enter the element at {i} index in the list: "))
     l.append(p)
-print(f"Original List is : {l}")
+# print(f"Original List is : {l}")
 print(f"Sorted List is: {ins_sort(l)}")
